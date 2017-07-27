3650b6bf01d7fd40ca25fe85e5784ecdba2282fa
wenhanglei <3188744608@qq.com>
2017-07-27 14:30:38 +0800

    improve some files

--- scripts/showPic.js
+++ scripts/showPic.js
@@ -1,14 +1,36 @@
+function prepareGallery() {
+	if (!document.getElementsByTagName) return false;
+	if (!document.getElementById) return false;
+	if (!document.getElementById("imagegallery")) return false;
+	var gallery = document.getElementById("imagegallery");
+	var links = gallery.getElementsByTagName("a");
+	for (var i=0; i < links.length; i++) {
+		links[i].onclick = function() {
+			return showPic(this) ? false : true;
+		}
+	}
+
+}
+
 function showPic(whichpic) {
+	if (!document.getElementById("Placeholder")) return false;
 	var source = whichpic.getAttribute("href");
 	var placeholder = document.getElementById("placeholder");
+	if (placeholder.nodeName != "IMG") return false;
 	placeholder.setAttribute("src", source);
-	var text = whichpic.getAttribute("title");
-	var description = document.getElementById("description");
-	description.firstChild.nodeValue = text
+	if (document.getElementById("dscription")) {
+		var text = whichpic.getAttribute("title") ? whichpic.getAttribute("title") : "";
+		var description = document.getElementById("description");
+		if (description.firstChild.nodeType == 3) {
+			description.firstChild.nodeValue = text
+		}
+	}
+	return true;
 }
 
 function countBodyChildren() {
 	var body_element = document.getElementsByTagName("body")[0];
 	alert(body_element.childNodes.length);
 }
-window.onload = countBodyChildren;
+
+addloadEvent(prepareGallery);
