1931e3673edb4e615c87813d3633e81a47c11c18
wenhanglei <3188744608@qq.com>
2017-07-27 16:24:25 +0800

    some more improvement on gallery

--- scripts/example.js
+++ scripts/example.js
@@ -1,5 +1,7 @@
-mood = 'happy'
-age = 24
-
-//alert(mood)
-alert(age)
+window.onload = function() {
+	var para = document.createElement("p");
+	var testdiv = document.getElementById("testdiv");
+	testdiv.appendChild(para);
+	var txt = document.createTextNode("Hello world");
+	para.appendChild(txt);
+}
