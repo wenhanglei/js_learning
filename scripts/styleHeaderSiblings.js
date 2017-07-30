feda3aec22d41a2bfe69b4326ca22b702b1c808e
wenhanglei <3188744608@qq.com>
2017-07-30 13:32:44 +0800

    separate the presentation and behaviour

--- scripts/styleHeaderSiblings.js
+++ scripts/styleHeaderSiblings.js
@@ -4,8 +4,7 @@ function styleHeaderSiblings() {
 	var elem;
 	for (var i=0; i<headers.length; i++) {
 		elem = getNextElement(headers[i].nextSibling);
-		elem.style.fontWeight = "bold";
-		elem.style.fontSize = "1.2em";
+		addClass(elem, "intro");
 	}
 }
 function getNextElement(node) {
